21a1b1caad6da46e53b3ca792f4a9fad30fb99e6
Suddani <suddani@googlemail.com>
2016-03-08 22:42:01 +0100

    only generate a spritesheet if we have frames, otherwise generate a image

--- games/sudi/src/spritesheet_generator.js
+++ games/sudi/src/spritesheet_generator.js
@@ -29,7 +29,11 @@ SpritesheetGenerator.prototype.createSpriteSheet = function(atlas_key, frame_nam
   var orb = this.game.make.sprite(0, 0, atlas_key, frame.name);
   var bmd = this.game.add.bitmapData(frame.width, frame.height);
   bmd.draw(orb, 0, 0);
-  this.game.cache.addSpriteSheet(frame.name, '', bmd.canvas, frame.height, frame.height, frame.width/frame.height, 0, 0);
+  //Check if there is actually more than one frame. otherwise just create this one image
+  if (this.game.cache.getFrameData(atlas_key).getFrameByName(frame_name+"_0000"))
+    this.game.cache.addSpriteSheet(frame.name, '', bmd.canvas, frame.height, frame.height, frame.width/frame.height, 0, 0);
+  else
+    this.game.cache.addImage(frame.name, '', bmd.canvas);
 }
 
 return new SpritesheetGenerator();
